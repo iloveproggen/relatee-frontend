bd04aa5067f0a61872d7379f8972895b9d261724
Mauriceha02 <98476338+Mauriceha02@users.noreply.github.com>
2024-04-18 16:14:46 +0200

    übersetzung profile

--- frontend_v1/lib/assets/LocaleStrings.dart
+++ frontend_v1/lib/assets/LocaleStrings.dart
@@ -8,14 +8,26 @@ class LocaleString extends Translations {
           'welcome_message': 'Time to get productive.',
           'back_button_text': 'back',
           'Shop_title': 'Shop',
-          'Shop_info': 'Spend your hard earned points for goodies'
+          'Shop_info': 'Spend your hard earned points for goodies',
+          'part_household': 'part of household ',
+          'view_txt': 'view',
+          'overdue_txt': 'overdue',
+          'due_in_txt': 'due in',
+          'day_txt': 'day',
+          'days_txt': 'days',
         },
         'de-DE': {
           'welcome_title': 'Willkommen',
           'welcome_message': 'Es ist Zeit, produktiv zu sein.',
           'back_button_text': 'zurück',
           'Shop_title': 'Shop',
-          'Shop_info': 'Geben Sie Ihre hart verdienten Punkte für Goodies aus'
+          'Shop_info': 'Geben Sie Ihre hart verdienten Punkte für Goodies aus',
+          'part_household': 'Teil des Haushalts ',
+          'view_txt': 'siehe',
+          'overdue_txt': 'überfällig',
+          'due_in_txt': 'fällig in',
+          'day_txt': 'Tag',
+          'days_txt': 'Tagen',
         }
       };
 }
