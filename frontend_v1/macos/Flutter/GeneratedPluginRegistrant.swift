5a78031f5be5e036229739d9a4a3d6c8553d6a03
Mauriceha02 <98476338+Mauriceha02@users.noreply.github.com>
2024-04-19 08:31:34 +0200

    Bug fix

--- frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import devicelocale
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
