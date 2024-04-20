94cd9d9eddbdaf50f4b2dc9a9f5921711631aca9
Michelle Gerwald <149069955+iloveproggen@users.noreply.github.com>
2024-04-20 10:22:07 +0200

    uploaded new asset

--- frontend_v1/lib/main.dart
+++ frontend_v1/lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:frontend_v1/household_tasks.dart';
