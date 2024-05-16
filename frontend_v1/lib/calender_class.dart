74172b0444beea5a34601e96ff851ba24988c6b3
Maurice <98476338+Mauriceha02@users.noreply.github.com>
2024-05-16 13:36:07 +0200

    calender class

---
+++ frontend_v1/lib/calender_class.dart
@@ -0,0 +1,26 @@
+import 'package:intl/intl.dart';
+
+//purpose: Class to get the current date, month, year and time
+//author: Maurice Halilovic
+//Date: 16.05.2024
+class Calendar {
+  int getCurrentDay() {
+    DateTime now = DateTime.now();
+    return now.day;
+  }
+
+  String getCurrentMonth() {
+    DateTime now = DateTime.now();
+    return DateFormat('MMMM').format(now);
+  }
+
+  int getCurrentYear() {
+    DateTime now = DateTime.now();
+    return now.year;
+  }
+
+  String getCurrentTime() {
+    DateTime now = DateTime.now();
+    return DateFormat('HH:mm').format(now);
+  }
+}
