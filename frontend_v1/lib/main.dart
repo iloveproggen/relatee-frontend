c7a3700f9c49b518b17201cb74e1de36d27b92cc
Michelle Gerwald <149069955+iloveproggen@users.noreply.github.com>
2024-06-13 20:41:36 +0200

    small changes to color

--- frontend_v1/lib/main.dart
+++ frontend_v1/lib/main.dart
@@ -238,6 +238,9 @@ Future<Map<String, dynamic>> getUserData() async {
         mappedResult['points'] = 0;
       }
       
+    if (mappedResult['color'] != "#000000") {
+      mappedResult['color'] = "#00000000";
+    }
 
       return mappedResult;
     }
