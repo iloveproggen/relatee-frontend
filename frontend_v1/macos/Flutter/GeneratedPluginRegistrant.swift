a5e65c00da9edb662bd140b54dd3e8e739037c64
ReneIU <149189871+ReneIU@users.noreply.github.com>
2024-06-27 11:10:27 +0200

    - Umlaute in Register eingebaut

--- frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
