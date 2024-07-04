43d92094718c897031992f881d69335e3bb112da
Maurice <98476338+Mauriceha02@users.noreply.github.com>
2024-07-04 10:35:21 +0200

    Update GeneratedPluginRegistrant.swift

--- frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,12 @@ import Foundation
 
 import connectivity_plus
 import devicelocale
-import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
