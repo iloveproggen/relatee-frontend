c079f884dc7e4e86d2f738310352f9ec4efde372
Michelle Gerwald <migerwald@gmail.com>
2026-07-01 08:40:06 +0200

    hallelujah es hat angefangen zu funktionieren!!!

--- frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_v1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,10 @@ import Foundation
 
 import connectivity_plus
 import devicelocale
-import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
